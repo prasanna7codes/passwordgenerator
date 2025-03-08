362a2c13e4f644c5da799e9ed0941392bdd313cd
prasanna7codes <prasannalit098@gmail.com>
2025-03-08 12:56:41 +0530

    using usestate to manage the variables

--- App.tsx
+++ App.tsx
@@ -1,16 +1,40 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { object, string, number, date, InferType } from 'yup';
 
 
 const PasswordSchema = object().shape({
-  passwordlength: number().required('Length is required').positive().integer().min(4,'should be minimum of 4 characters').max(20,'should be maximum of 20 characters'),
+  passwordLength: number().required('Length is required').positive().integer().min(4,'should be minimum of 4 characters').max(20,'should be maximum of 20 characters'),
 });
 
 
 
 
 export default function App() {
+  
+  const [password,setPassword]=useState('');//the password has a default value of an empty string bcoz we used  '' in the useState function
+
+  const [isPassGenerated,setIsPassGenerated]=useState(false);
+
+  const [lowercase,setlowerCase]=useState(false);
+  const [uppercase,setUpperCase]=useState(false);
+  const [numbers,setNumbers]=useState(false);
+  const [symbols,setSymbols]=useState(false);
+
+
+  const generatePasswordString=(passwordLength:number)=>{
+    //
+  }
+  
+
+  const CreatePassword=(characters:string,passwordLength:number)=>{
+    //
+  }
+
+  const resetPassword=()=>{
+    //
+  }
+
   return (
     <View>
       <Text>App again
